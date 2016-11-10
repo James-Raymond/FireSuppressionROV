fa330896a543e19aff21b83678f26d4d9b5f37c5
James-Raymond <jamesmraymond7@gmail.com>
2016-11-10 19:18:53 +0000

    Add files via upload
    Adding to database

--- GPS.py
+++ GPS.py
@@ -5,11 +5,13 @@
 
 import smbus
 import time
-import smbus								
-import time
+import MySQLdb						
 
 bus = smbus.SMBus(1)
 address = 0x68	#I2C address (A0 & A1 jumpers ON)			
+database = MySQLdb.connect(host='localhost',user='root',passwd='root',db='GPS')# link to database
+
+cursor = database.cursor()
 
 			
 # single register from GPM.S
@@ -38,18 +40,18 @@ def get_double(register):
 while True:
 
 	# Time
-        hours = get_double(0)
-        minutes = get_double(2)
-        seconds = get_double(4)
-        print"time:  ", hours,":",minutes,":",seconds
+        hours = float(get_double(0))
+        minutes =float( get_double(2))
+        seconds = float( get_double(4))
+	time_gps ="%.0f:%0.f:%.0f"%(hours,minutes,seconds)
+	print'Time: ',time_gps
 
         # Date
-        day = get_double(6)
-        months = get_double(8)
-        years = get_double(12)
-        print "Date:",day,"/",months,"/",years
-
-
+        day = float(get_double(6))
+        months =float(get_double(8))
+        years = float(get_double(12))
+        date = "%.0f-%0.f-%.0f"%(day,months,years)
+	print 'Date: ', date
 
 	# Headind
         heading = float(get_single(44))*100
@@ -57,6 +59,7 @@ while True:
         heading += float(get_single(46))
         heading += float(get_single(47))/10
         print "heading:",heading
+
 	# Latitude 
         latitude_degrees = get_double(14)
         latitude_minutes = float(get_double(16))
@@ -65,7 +68,9 @@ while True:
         latitude_minutes += float(get_single(20))/1000
 	latitude_minutes += float(get_single(21))/10000
 	latitude_direction = chr(get_single(22))
-	print "Latitude: ", latitude_degrees ," " ,latitude_minutes ," " ,latitude_direction
+	latitude ="%.0f%.5f %s" %(latitude_degrees,latitude_minutes,latitude_direction)
+        print 'latitude: ',latitude
+
 	# Longitude 
         longitude_degrees = get_single(23)*100
         longitude_degrees += get_double(24)
@@ -75,10 +80,16 @@ while True:
         longitude_minutes += float(get_single(30))/1000
         longitude_minutes += float(get_single(31))/10000
         longitude_direction = chr(get_single(32))
-        print "longitude: " , longitude_degrees , " " , longitude_minutes ," ", longitude_direction
-
+        longitude ="%.0f%.5f %s" %(longitude_degrees,longitude_minutes,longitude_direction)
+	print 'longitude: ',longitude
 	print "\n\n"
+        cursor.execute(''' INSERT INTO  data (time , date , latitude , longitude , heading) VALUES (%s,%s,%s,%s,%s) ''',(time_gps, date, latitude, longitude, heading))
+
+        database.commit()
+
         time.sleep(1)
+
+	
 
 
 
