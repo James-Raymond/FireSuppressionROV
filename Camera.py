54f506ea56d6b6f0a7a554c15ea81848b0b14706
James-Raymond <jamesmraymond7@gmail.com>
2016-12-10 17:30:16 +0000

    Add files via upload
    slight changes

--- Camera.py
+++ Camera.py
@@ -1,10 +1,16 @@
+#To check if the camera is connected run
+#	vcgencmd get_camera
+#Type into VLC in network stream
+#	 tcp/h264://my_pi_address:8000/
+
+
 import socket
 import time
 import picamera
 
 with picamera.PiCamera() as camera:
 	camera.resolution = (640,480)
-	camera.framerate = 24
+	camera.framerate = 25
 
 	server_socket = socket.socket()
 	server_socket.bind(('0.0.0.0',8000))
@@ -14,7 +20,7 @@ with picamera.PiCamera() as camera:
 	connection = server_socket.accept()[0].makefile('wb')
 	try:
 		camera.start_recording(connection, format='h264')
-		camera.wait_recording(60)
+		camera.wait_recording(1000)
 		camera.stop_recording()
 
 	finally:
