2a992aaca94d95e9b4ee698dd27d968cacd88db9
James-Raymond <jamesmraymond7@gmail.com>
2017-03-26 18:24:26 +0100

    Add files via upload
    These files are layout and the main . Using Async task i am connecting to a socket in the background and am sending the information to the server side

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,79 @@
-package com.example.james.myapplication;
+package com.example.james.servotesting;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 import android.widget.SeekBar;
-import android.widget.TextView;
-import android.support.v7.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+import java.io.*;
+import java.net.Socket;
+
+public class MainActivity extends Activity {
+
+
+    EditText ipText;
+    EditText portText;
+    Button button;
+    int progressBar;
+    private Socket socket=null;
+    private String ipaddress;
+    private String port;
+    public PrintWriter pw;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar forward = (SeekBar) findViewById(R.id.seekBar2);
-        SeekBar left  = (SeekBar) findViewById(R.id.seekBar);
+        ipText = (EditText) findViewById(R.id.ipText);
+        portText = (EditText) findViewById(R.id.portText);
+        final TextView text2 =(TextView) findViewById(R.id.seekBarText);
+        button = (Button) findViewById(R.id.button);
+        final SeekBar left = (SeekBar) findViewById(R.id.seekBar1);
 
 
-        final TextView text1=(TextView) findViewById(R.id.textView);
-        final TextView text2=(TextView) findViewById(R.id.textView2);
+        button.setOnClickListener(new View.OnClickListener() {
 
-        forward.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,
-                                          boolean fromUser) {
-                // TODO Auto-generated method stub
-                text1.setText(String.valueOf(progress));
-            }
+            private ProgressDialog pd = null;
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
+            public void onClick(View v) {
+               ipaddress=ipText.getText().toString();
+                port=portText.getText().toString();
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
+
+
             }
         });
+        left.setOnSeekBarChangeListener(new SeekBar .OnSeekBarChangeListener() {
 
-        left.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,
+            public void onProgressChanged(SeekBar seekBar1, int progress,
                                           boolean fromUser) {
                 // TODO Auto-generated method stub
                 text2.setText(String.valueOf(progress));
+                progressBar = progress+2;
+                try {
+                    new AsyncAction().execute();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
+
                 // TODO Auto-generated method stub
             }
 
@@ -60,6 +83,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private class AsyncAction extends AsyncTask<String,Void,String>{
 
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                socket = new Socket(ipaddress, Integer.parseInt(port));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            PrintWriter outToServer = null;
+            try {
+                outToServer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            outToServer.print(progressBar);
+            outToServer.flush();
+            return null;
+        }
     }
 }
+
+
